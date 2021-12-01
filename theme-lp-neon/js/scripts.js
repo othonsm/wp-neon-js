2826ae08e03c74b9197de0c0b78cf9a549432f65
Othon Medeiros <othonsmedeiros@gmail.com>
2021-12-01 15:41:10 -0300

    Commit inicial

---
+++ theme-lp-neon/js/scripts.js
@@ -0,0 +1,8 @@
+/* const btnDropdown = document.querySelector('.js-btn-dropdown');
+const dropdown = document.querySelector('.js-dropdown');
+
+function openDropdown() {
+    console.log('ativar função');
+}
+
+btnDropdown.addEventListener('click', openDropdown); */
